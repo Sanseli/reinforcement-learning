fef556ac8b7b215f1f0dc1037e50496bcdce0f52
Lisa <lisasansen97@gmail.com>
2019-11-25 10:45:02 +0100

    :sparkles: get solutions by http request

--- pages/sliding-puzzle.vue
+++ pages/sliding-puzzle.vue
@@ -2,6 +2,8 @@
   <v-container fluid>
     <v-row justify="center" style="height: 3em;">
       <h1>{{getMessage()}}</h1>
+
+      {{this.grid}}
     </v-row>
     <label hidden>{{ count}}</label>
     <v-row style="padding-top: 5em">
@@ -44,6 +46,9 @@
             <v-btn block @click="solve" :disabled="shuffling">
               Solve
             </v-btn>
+            <v-btn block @click="solveRequest" >
+              Solve request
+            </v-btn>
           </v-col>
         </v-row>
       </v-col>
@@ -83,20 +88,7 @@
 
                 grid: [[1, 2], [3, 0]],
 
-                solutions: [
-                    {"configuration": [[2, 0], [1, 3]], "action": "Left"},
-                    {"configuration": [[2, 3], [0, 1]], "action": "Right"},
-                    {"configuration": [[3, 1], [0, 2]], "action": "Up"},
-                    {"configuration": [[2, 3], [1, 0]], "action": "Up"},
-                    {"configuration": [[3, 0], [2, 1]], "action": "Down"},
-                    {"configuration": [[0, 1], [3, 2]], "action": "Right"},
-                    {"configuration": [[1, 2], [3, 0]], "action": "Down"},
-                    {"configuration": [[1, 0], [3, 2]], "action": "Down"},
-                    {"configuration": [[3, 1], [2, 0]], "action": "Left"},
-                    {"configuration": [[1, 2], [0, 3]], "action": "Right"},
-                    {"configuration": [[0, 2], [1, 3]], "action": "Down"},
-                    {"configuration": [[0, 3], [2, 1]], "action": "Down"}
-                ],
+                solutions: [],
 
                 snackbar: false,
                 count: 0,
@@ -127,6 +119,36 @@
                     } else this.shuffling = false
                 }, 200)
 
+            },
+            async solveRequest() {
+
+                this.solutions = await this.$axios.$get(`http://localhost:8881/solve?grid=[[${this.grid[0][0]}, ${this.grid[0][1]}], [${this.grid[1][0]}, ${this.grid[1][1]}]]`)
+                console.log(this.solutions)
+                this.count += 1
+
+                if (
+                    this.grid[0][0] === 1 &&
+                    this.grid[0][1] === 2 &&
+                    this.grid[1][0] === 3 &&
+                    this.grid[1][1] === 0
+                ) {
+                    this.solved = true;
+                }
+                else {
+                    const solution = this.findAction();
+
+                    if (solution === false) {
+                        this.snackbar = true;
+                        setTimeout(()=> {
+                            this.shuffle()
+                        }, 1000)
+                    }
+                    else {
+                        setTimeout(()=> {
+                            this.switchNumbers(solution.action, 'solve')
+                        }, 1000)
+                    }
+                }
             },
             solve() {
                 this.count += 1
