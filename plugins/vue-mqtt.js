4dc9500f4d5bb49691aecd6a0e5018c70b66ef41
Lisa <lisasansen97@gmail.com>
2019-11-28 16:00:00 +0100

    ip:

--- plugins/vue-mqtt.js
+++ plugins/vue-mqtt.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueMqtt from 'vue-mqtt';
 
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://10.150.138.8:9001')
-// var client  = mqtt.connect('mqtt://localhost:9001')
+// var client  = mqtt.connect('mqtt://10.150.138.8:9001')
+var client  = mqtt.connect('mqtt://localhost:9001')
 
 Vue.use(VueMqtt, client);
