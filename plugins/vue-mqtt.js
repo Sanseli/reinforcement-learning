13890c94e759208c03fa04b706020bc8657b6c43
Bram Vandendriessche <BramVandendriessche@users.noreply.github.com>
2019-11-29 20:01:38 +0100

    mqtt port to 80 (for ngrok tunnel)

--- plugins/vue-mqtt.js
+++ plugins/vue-mqtt.js
@@ -5,7 +5,7 @@ import file from '!raw-loader!~/assets/tunnel-domain.txt'
 console.log(file)
 var mqtt = require('mqtt')
 // var client  = mqtt.connect('mqtt://10.150.138.90:9001')
-var client  = mqtt.connect(`mqtt://${file}:9001`)
+var client  = mqtt.connect(`mqtt://${file}:80`)
 
 Vue.use(VueMqtt, client);
 
